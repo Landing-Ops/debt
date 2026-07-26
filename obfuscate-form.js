73d2c5a5c970773b5475aed0f45905301972da86
minwookang5687 <softman007@naver.com>
2026-07-26 23:21:14 +0900

    g
    g

--- obfuscate-form.js
+++ obfuscate-form.js
@@ -3,8 +3,7 @@ const { execSync } = require("child_process");
 const glob = require("glob");
 const path = require("path");
 
-// js/ 폴더 및 하위 폴더에서 "-nude.js"로 끝나는 모든 파일 검색
-const files = glob.sync("js/**/*-nude.js");
+const files = glob.sync("js/*-*-nude.js");
 
 if (files.length === 0) {
   console.log("⚠️  처리할 *-nude.js 파일이 없습니다.");
@@ -17,22 +16,24 @@ files.forEach((file) => {
   console.log(`➡️  난독화: ${file} → ${outFile}`);
 
   execSync(
-    `javascript-obfuscator ${file} --output ${outFile} \
-    --compact true \
-    --self-defending true \
-    --string-array true \
-    --string-array-threshold 1 \
-    --string-array-encoding base64 \
-    --control-flow-flattening true \
-    --control-flow-flattening-threshold 0.8 \
-    --dead-code-injection true \
-    --dead-code-injection-threshold 0.2 \
-    --numbers-to-expressions true \
-    --identifier-names-generator mangled \
-    --split-strings true \
-    --split-strings-chunk-length 5`,
-    { stdio: "inherit" }
-  );
+  `javascript-obfuscator ${file} --output ${outFile} \
+  --compact true \
+  --self-defending true \
+  --string-array true \
+  --string-array-threshold 1 \
+  --string-array-encoding base64 \
+  --control-flow-flattening true \
+  --control-flow-flattening-threshold 0.8 \
+  --dead-code-injection true \
+  --dead-code-injection-threshold 0.2 \
+  --numbers-to-expressions true \
+  --identifier-names-generator mangled`
+  ,
+  { stdio: "inherit" }
+);
 });
 
-console.log("✅ 모든 *-nude.js 난독화 완료");
+console.log("✅ 모든 form-*-nude.js 난독화 완료");
+
+// --split-strings true \
+// --split-strings-chunk-length 10
