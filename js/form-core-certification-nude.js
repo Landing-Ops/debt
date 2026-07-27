dc8368309aa1883684eae329c61ee009862465b9
minwookang5687 <softman007@naver.com>
2026-07-28 02:59:24 +0900

    g
    g

--- js/form-core-certification-nude.js
+++ js/form-core-certification-nude.js
@@ -159,7 +159,7 @@
     otpBox = document.createElement('div');
     otpBox.setAttribute('data-otp-box', '');  
     otpBox.innerHTML =
-      '<p style="font-size:14px;color:#d33;margin-bottom:4px; text-align:left;">타인 번호 도용방지를 위해 번호 인증을 시행하고 있습니다.</p>' + // 번호인증 카피 ) 타인 번호 도용 방지를 위해 번호인증을 시행하고 있습니다. / 정확한 탕감액 산정을 위해 번호 인증을 진행해주세요.
+      '<p style="font-size:14px;color:#d33;margin-bottom:4px; text-align:left;">타인 번호 도용 방지를 위해 번호 인증을 시행하고 있습니다.</p>' + // 번호인증 카피 ) 타인 번호 도용 방지를 위해 번호인증을 시행하고 있습니다. / 정확한 탕감액 산정을 위해 번호 인증을 진행해주세요.
       '<div class="otp-row">' +
         '<input data-otp-code type="text" maxlength="6" inputmode="numeric" ' +
           'pattern="[0-9]*" autocomplete="off" ' +
