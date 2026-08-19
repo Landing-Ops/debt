0607c344bd66a4548d41a3182eec05bd045e05c8
minwookang5687 <softman007@naver.com>
2026-08-19 18:55:03 +0900

    d
    d

--- js/form-core-certification-nude.js
+++ js/form-core-certification-nude.js
@@ -403,7 +403,7 @@
     // if (v.message.length < 2) { f.message.classList.add('is-invalid'); return { ok: false, msg: '문의사항을 입력하세요.' }; }
     if (!f.agree.checked) { f.agree.classList.add('is-invalid'); return { ok: false, msg: '개인정보 동의를 해주세요.' }; }
 
-    return { ok: true, msg: '예상탕감액 진단 신청하기' };
+    return { ok: true, msg: '최대 예상탕감액 안내받기' };
   }
 
   /* =====================================================================
